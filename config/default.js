b5c6347ed715c79c3f834997e473d6272141c14e
Martynas Bardauskas <martynas.bardauskas@gmail.com>
2015-08-27 18:30:23 +0300

    added overridable config which would not be tracked

---
+++ config/default.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var merge = require('merge');
+var overridesFilepath = __dirname + '/overrides.js';
+
+var config = {
+	personID: 38704235897,
+	requestID: 458425,
+	requestURL: 'https://www.eregitra.lt/viesa/interv/INT_Header_Inf.php',
+	userAgent: 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+};
+
+if(fs.existsSync(overridesFilepath)) {
+	var overrides = require(overridesFilepath);
+	config = merge(config, overrides);
+}
+
+module.exports = config;
