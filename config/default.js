3548549e702c9dbca68c82b8e133b0dec3862007
Martynas Bardauskas <martynas.bardauskas@gmail.com>
2015-08-27 18:59:57 +0300

    added functionality which goes to regitra page, searches for your person ID, then enters request ID and solved captcha to see available exams. for now everything is saved in a screenshot

--- config/default.js
+++ config/default.js
@@ -6,7 +6,9 @@ var config = {
 	personID: 38704235897,
 	requestID: 458425,
 	requestURL: 'https://www.eregitra.lt/viesa/interv/INT_Header_Inf.php',
-	userAgent: 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+	userAgent: 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+	captchaFile: __dirname + '/../.temp/captcha.jpg',
+	screenshotFile: __dirname + '/../.temp/page.png'
 };
 
 if(fs.existsSync(overridesFilepath)) {
---
+++ index.js
@@ -0,0 +1,22 @@
+var Horseman = require('node-horseman');
+var solver = require('./lib/solver');
+var config = require('./config/default');
+
+//process.env['TESSDATA_PREFIX'] = 'c:/server/Tesseract-OCR';
+
+var horseman = new Horseman();
+horseman
+	.userAgent(config.userAgent)
+	.open(config.requestURL)
+	.type('form input[name="P_Asm_kodas"]', String(config.personID))
+	// note that coordinates might change. click method doesn't work
+	.mouseEvent('click', 575, 55)
+	.waitForNextPage()
+	.crop('#imgCaptcha', config.captchaFile)
+	.type('input#PRASYMO_NR_P0', String(config.requestID))
+	.type('input#code0', solver.solveCaptcha(config.captchaFile))
+	// clicks on submit button
+	g.click('.CTL_PLACE_LBL .yellow-btn input[type="button"]')
+	.waitForNextPage()
+	.screenshot(config.screenshotFile)
+	.close();
